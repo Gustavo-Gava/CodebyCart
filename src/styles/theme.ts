1e4a9bc51ab58abae71017bf601fe8efe5025092
Gustavo Gava <gustavogava123@hotmail.com>
2022-05-19 18:46:50 -0300

    feat: added styles configuration

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "300": "#d1d8e4",
+      "500": "#C4C4C4",
+    },
+    blue: {
+      "500": "#3c74ed"
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.300",
+        fontFamily: "Poppins"
+      },
+    }
+  }
+})
